311741678b574043458db1c50c17349ce6985d62
Waqas-Shah-42 <wshah052@uottawa.ca>
2023-03-24 00:29:34 +0500

    healthcheck now returns json format responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w,"states: available")
-	fmt.Fprintf(w,"environment: %s\n", app.config.env)
-	fmt.Fprintf(w,"version: %s\n", version)
+	
+	data := map[string]string{
+		"status": "available",
+		"environment": app.config.env,
+		"version": version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered while encoding json", http.StatusInternalServerError)
+		return
+	}
+
+
+	// js := `{"status": available, environment": %q, "version": %q}`
+	// js = fmt.Sprintf(js, app.config.env, version)
+
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Write([]byte(js))
+
+	// fmt.Fprintln(w,"states: available")
+	// fmt.Fprintf(w,"environment: %s\n", app.config.env)
+	// fmt.Fprintf(w,"version: %s\n", version)
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -13,7 +14,25 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 
 	id, err := strconv.ParseInt(params.ByName("id"),10,64)
 	if err != nil || id < 1 {
-		return 0, errors.New("Invalid id parameter")
+		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
