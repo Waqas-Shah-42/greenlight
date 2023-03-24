1290ef32c96befe392e3c6341097f436fd37281b
Waqas-Shah-42 <wshah052@uottawa.ca>
2023-03-24 16:54:13 +0500

    Changed error responses to json format

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -15,17 +15,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	err := app.writeJSON(w, http.StatusOK, envelope{"system_info": data}, nil)
 	if err != nil {
 		app.logger.Println(err)
-		http.Error(w, "The server encountered while encoding json", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
-
-	// js := `{"status": available, environment": %q, "version": %q}`
-	// js = fmt.Sprintf(js, app.config.env, version)
-
-	// w.Header().Set("Content-Type", "application/json")
-	// w.Write([]byte(js))
-
-	// fmt.Fprintln(w,"states: available")
-	// fmt.Fprintf(w,"environment: %s\n", app.config.env)
-	// fmt.Fprintf(w,"version: %s\n", version)
 }
